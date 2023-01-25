832af5efd2c0755bcfc512c963eab0e8c3278585
Magomedali Gajiev <mag198421@gmail.com>
2023-01-25 12:50:11 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIdElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Cheers, it's MASHA!");
+}
+else
+{
+    Console.WriteLine("Hello, " + username + "!");
+}
